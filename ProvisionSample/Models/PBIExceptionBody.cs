7f1913d066650f48459ea5eac0d5ac0701afafcd
madia <madia@microsoft.com>
2017-01-18 16:45:19 +0200

    extract error message from response body

---
+++ ProvisionSample/Models/PBIExceptionBody.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace ProvisionSample.Models
+{
+    public class PBIExceptionBody
+    {
+        [JsonProperty(PropertyName = "error")]
+        public PBIError Error { get; set; }
+    }
+
+    public class PBIError
+    {
+        [JsonProperty(PropertyName = "code")]
+        public string Code { get; set; }
+
+        [JsonProperty(PropertyName = "details")]
+        public IEnumerable<ExceptionDetails> Details { get; set; }
+    }
+
+    public class ExceptionDetails
+    {
+        [JsonProperty(PropertyName = "message")]
+        public string Message { get; set; }
+    }
+}
